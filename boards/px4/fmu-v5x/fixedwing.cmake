jannsta1/Firmware

fmuv5x board support

boards/px4/fmu-v5x/fixedwing.cmake
@@ -1,0 +1,88 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR px4
+	MODEL fmu-v5
+	LABEL fixedwing
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m7
+	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	UAVCAN_INTERFACES 2
+
+	SERIAL_PORTS
+		GPS1:/dev/ttyS0
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		TEL4:/dev/ttyS3
+
+	DRIVERS
+		barometer # all available barometer drivers
+		batt_smbus
+		camera_capture
+		camera_trigger
+		differential_pressure # all available differential pressure drivers
+		distance_sensor # all available distance sensor drivers
+		gps
+		imu/adis16448
+		imu/bmi055
+		imu/mpu6000
+		lights/rgbled
+		lights/rgbled_ncp5623c
+		lights/rgbled_pwm
+		magnetometer # all available magnetometer drivers
+		pwm_input
+		pwm_out_sim
+		px4fmu
+		px4io
+		rc_input
+		stm32
+		stm32/adc
+		stm32/armv7-m_dcache
+		stm32/tone_alarm
+		telemetry # all available telemetry drivers
+		tone_alarm
+		uavcan
+
+	MODULES
+		camera_feedback
+		commander
+		dataman
+		ekf2
+		events
+		fw_att_control
+		fw_pos_control_l1
+		land_detector
+		load_mon
+		logger
+		mavlink
+		navigator
+		sensors
+		vmount
+		wind_estimator
+
+	SYSTEMCMDS
+		bl_update
+		config
+		dumpfile
+		esc_calib
+		hardfault_log
+		led_control
+		mixer
+		motor_ramp
+		motor_test
+		mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		reflect
+		sd_bench
+		shutdown
+		top
+		topic_listener
+		tune_control
+		usb_connected
+		ver
+	)

boards/px4/fmu-v5x/multicopter.cmake
@@ -1,0 +1,98 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR px4
+	MODEL fmu-v5
+	LABEL multicopter
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m7
+	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
+	UAVCAN_INTERFACES 2
+
+	SERIAL_PORTS
+		GPS1:/dev/ttyS0
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		TEL4:/dev/ttyS3
+
+	DRIVERS
+		barometer # all available barometer drivers
+		batt_smbus
+		camera_capture
+		camera_trigger
+		distance_sensor # all available distance sensor drivers
+		gps
+		imu/bmi055
+		imu/mpu6000
+		irlock
+		lights/blinkm
+		lights/oreoled
+		lights/rgbled
+		lights/rgbled_ncp5623c
+		lights/rgbled_pwm
+		magnetometer # all available magnetometer drivers
+		pmw3901
+		pwm_input
+		pwm_out_sim
+		px4flow
+		px4fmu
+		px4io
+		rc_input
+		roboclaw
+		stm32
+		stm32/adc
+		stm32/armv7-m_dcache
+		stm32/tone_alarm
+		tap_esc
+		telemetry # all available telemetry drivers
+		tone_alarm
+		uavcan
+
+	MODULES
+		attitude_estimator_q
+		camera_feedback
+		commander
+		dataman
+		ekf2
+		events
+		land_detector
+		landing_target_estimator
+		load_mon
+		local_position_estimator
+		logger
+		mavlink
+		mc_att_control
+		mc_pos_control
+		navigator
+		sensors
+		sih
+		vmount
+		wind_estimator
+
+	SYSTEMCMDS
+		bl_update
+		config
+		dumpfile
+		esc_calib
+		hardfault_log
+		led_control
+		mixer
+		motor_ramp
+		motor_test
+		mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		reflect
+		sd_bench
+		shutdown
+		top
+		topic_listener
+		tune_control
+		usb_connected
+		ver
+	)

boards/px4/fmu-v5x/rover.cmake
@@ -1,0 +1,92 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR px4
+	MODEL fmu-v5
+	LABEL rover
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m7
+	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	UAVCAN_INTERFACES 2
+
+	SERIAL_PORTS
+		GPS1:/dev/ttyS0
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		TEL4:/dev/ttyS3
+
+	DRIVERS
+		barometer # all available barometer drivers
+		batt_smbus
+		camera_capture
+		camera_trigger
+		distance_sensor # all available distance sensor drivers
+		gps
+		imu/bmi055
+		imu/mpu6000
+		lights/pca8574
+		lights/rgbled
+		lights/rgbled_ncp5623c
+		lights/rgbled_pwm
+		magnetometer # all available magnetometer drivers
+		#md25
+		mkblctrl
+		pca9685
+		pmw3901
+		pwm_input
+		pwm_out_sim
+		px4flow
+		px4fmu
+		px4io
+		rc_input
+		roboclaw
+		stm32
+		stm32/adc
+		stm32/armv7-m_dcache
+		stm32/tone_alarm
+		telemetry # all available telemetry drivers
+		tone_alarm
+		uavcan
+
+	MODULES
+		camera_feedback
+		commander
+		dataman
+		ekf2
+		events
+		gnd_att_control
+		gnd_pos_control
+		land_detector
+		load_mon
+		logger
+		mavlink
+		navigator
+		sensors
+		vmount
+
+	SYSTEMCMDS
+		bl_update
+		config
+		dumpfile
+		esc_calib
+		hardfault_log
+		led_control
+		mixer
+		motor_ramp
+		motor_test
+		mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		reflect
+		sd_bench
+		shutdown
+		top
+		topic_listener
+		tune_control
+		usb_connected
+		ver
+	)

boards/px4/fmu-v5x/rtps.cmake
@@ -1,0 +1,129 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR px4
+	MODEL fmu-v5
+	LABEL rtps
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m7
+	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
+	UAVCAN_INTERFACES 2
+
+	SERIAL_PORTS
+		GPS1:/dev/ttyS0
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		TEL4:/dev/ttyS3
+
+	DRIVERS
+		barometer # all available barometer drivers
+		batt_smbus
+		camera_capture
+		camera_trigger
+		differential_pressure # all available differential pressure drivers
+		distance_sensor # all available distance sensor drivers
+		gps
+		#heater
+		imu/adis16448
+		#imu # all available imu drivers
+		imu/bmi055
+		imu/mpu6000
+		imu/mpu9250
+		irlock
+		lights/blinkm
+		lights/oreoled
+		lights/pca8574
+		lights/rgbled
+		lights/rgbled_ncp5623c
+		lights/rgbled_pwm
+		magnetometer # all available magnetometer drivers
+		#md25
+		mkblctrl
+		pca9685
+		pmw3901
+		protocol_splitter
+		pwm_input
+		pwm_out_sim
+		px4flow
+		px4fmu
+		px4io
+		rc_input
+		roboclaw
+		stm32
+		stm32/adc
+		stm32/armv7-m_dcache
+		stm32/tone_alarm
+		tap_esc
+		telemetry # all available telemetry drivers
+		test_ppm
+		tone_alarm
+		uavcan
+
+	MODULES
+		attitude_estimator_q
+		camera_feedback
+		commander
+		dataman
+		ekf2
+		events
+		fw_att_control
+		fw_pos_control_l1
+		gnd_att_control
+		gnd_pos_control
+		land_detector
+		landing_target_estimator
+		load_mon
+		local_position_estimator
+		logger
+		mavlink
+		mc_att_control
+		mc_pos_control
+		micrortps_bridge
+		navigator
+		sensors
+		sih
+		vmount
+		vtol_att_control
+		wind_estimator
+
+	SYSTEMCMDS
+		bl_update
+		config
+		dumpfile
+		esc_calib
+		hardfault_log
+		led_control
+		mixer
+		motor_ramp
+		motor_test
+		mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		reflect
+		sd_bench
+		shutdown
+		tests # tests and test runner
+		top
+		topic_listener
+		tune_control
+		usb_connected
+		ver
+
+	EXAMPLES
+		bottle_drop # OBC challenge
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
+		#matlab_csv_serial
+		position_estimator_inav
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		segway
+		uuv_example_app
+	)

boards/px4/fmu-v5x/stackcheck.cmake
@@ -1,0 +1,129 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR px4
+	MODEL fmu-v5
+	LABEL stackcheck
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m7
+	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
+	#UAVCAN_INTERFACES 2
+
+	SERIAL_PORTS
+		GPS1:/dev/ttyS0
+		TEL1:/dev/ttyS1
+		TEL2:/dev/ttyS2
+		TEL4:/dev/ttyS3
+
+	DRIVERS
+		barometer # all available barometer drivers
+		batt_smbus
+		camera_capture
+		camera_trigger
+		differential_pressure # all available differential pressure drivers
+		distance_sensor # all available distance sensor drivers
+		gps
+		#heater
+		imu/adis16448
+		#imu # all available imu drivers
+		imu/bmi055
+		imu/mpu6000
+		imu/mpu9250
+		#irlock
+		#lights/blinkm
+		#lights/oreoled
+		lights/pca8574
+		lights/rgbled
+		#lights/rgbled_ncp5623c
+		lights/rgbled_pwm
+		magnetometer # all available magnetometer drivers
+		#md25
+		mkblctrl
+		pca9685
+		pmw3901
+		#protocol_splitter
+		pwm_input
+		pwm_out_sim
+		px4flow
+		px4fmu
+		px4io
+		rc_input
+		roboclaw
+		stm32
+		stm32/adc
+		stm32/armv7-m_dcache
+		stm32/tone_alarm
+		tap_esc
+		telemetry # all available telemetry drivers
+		test_ppm
+		tone_alarm
+		#uavcan
+
+	MODULES
+		attitude_estimator_q
+		camera_feedback
+		commander
+		dataman
+		ekf2
+		events
+		fw_att_control
+		fw_pos_control_l1
+		gnd_att_control
+		gnd_pos_control
+		land_detector
+		landing_target_estimator
+		load_mon
+		local_position_estimator
+		logger
+		mavlink
+		mc_att_control
+		mc_pos_control
+		#micrortps_bridge
+		navigator
+		sensors
+		sih
+		vmount
+		vtol_att_control
+		wind_estimator
+
+	SYSTEMCMDS
+		bl_update
+		config
+		dumpfile
+		esc_calib
+		hardfault_log
+		led_control
+		mixer
+		motor_ramp
+		motor_test
+		mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		reflect
+		sd_bench
+		shutdown
+		tests # tests and test runner
+		top
+		topic_listener
+		tune_control
+		usb_connected
+		ver
+
+	EXAMPLES
+		#bottle_drop # OBC challenge
+		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		#hello
+		#hwtest # Hardware test
+		#matlab_csv_serial
+		#position_estimator_inav
+		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		#rover_steering_control # Rover example app
+		#segway
+		#uuv_example_app
+	)

jn89b/afrl_px4

Former-commit-id: 31dfaee76ae4fad5327af60e54a291a45751e162

boards/px4/fmu-v5x/fixedwing.cmake
@@ -1,0 +1,99 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR px4
+	MODEL fmu-v5x
+	LABEL fixedwing
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m7
+	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	UAVCAN_INTERFACES 2
+
+	SERIAL_PORTS
+		GPS1:/dev/ttyS1
+		TEL1:/dev/ttyS6
+		TEL2:/dev/ttyS4
+		TEL3:/dev/ttyS2
+		GPS2:/dev/ttyS0
+
+	DRIVERS
+		barometer # all available barometer drivers
+		batt_smbus
+		camera_capture
+		camera_trigger
+		differential_pressure # all available differential pressure drivers
+		distance_sensor # all available distance sensor drivers
+		gps
+		#heater
+		imu/adis16448
+		imu/adis16497
+		#imu # all available imu drivers
+# TBD		imu/bmi088 - needs bus selection
+# TBD		imu/ism330dlc - needs bus selection
+		imu/mpu6000
+		irlock
+		lights/blinkm
+		lights/oreoled
+		lights/pca8574
+		lights/rgbled
+		lights/rgbled_ncp5623c
+		magnetometer # all available magnetometer drivers
+		optical_flow # all available optical flow drivers
+		power_monitor/ina226
+		pwm_input
+		pwm_out_sim
+		px4fmu
+		px4io
+		rc_input
+		stm32
+		stm32/adc
+		stm32/armv7-m_dcache
+		stm32/tone_alarm
+		telemetry # all available telemetry drivers
+		tone_alarm
+		uavcan
+
+	MODULES
+		camera_feedback
+		commander
+		dataman
+		ekf2
+		events
+		fw_att_control
+		fw_pos_control_l1
+		land_detector
+		load_mon
+		logger
+		mavlink
+		navigator
+		sensors
+		vmount
+		wind_estimator
+
+	SYSTEMCMDS
+		bl_update
+		config
+		dmesg
+		dumpfile
+		esc_calib
+		hardfault_log
+		led_control
+		mixer
+		motor_ramp
+		motor_test
+		mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		reflect
+		sd_bench
+		shutdown
+		top
+		topic_listener
+		tune_control
+		usb_connected
+		ver
+	)

boards/px4/fmu-v5x/multicopter.cmake
@@ -1,0 +1,105 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR px4
+	MODEL fmu-v5x
+	LABEL multicopter
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m7
+	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
+	UAVCAN_INTERFACES 2
+
+	SERIAL_PORTS
+		GPS1:/dev/ttyS1
+		TEL1:/dev/ttyS6
+		TEL2:/dev/ttyS4
+		TEL3:/dev/ttyS2
+		GPS2:/dev/ttyS0
+
+	DRIVERS
+		barometer # all available barometer drivers
+		batt_smbus
+		camera_capture
+		camera_trigger
+		distance_sensor # all available distance sensor drivers
+		gps
+		#heater
+		imu/adis16448
+		imu/adis16497
+		#imu # all available imu drivers
+# TBD		imu/bmi088 - needs bus selection
+# TBD		imu/ism330dlc - needs bus selection
+		imu/mpu6000
+		irlock
+		lights/blinkm
+		lights/oreoled
+		lights/pca8574
+		lights/rgbled
+		lights/rgbled_ncp5623c
+		magnetometer # all available magnetometer drivers
+		optical_flow # all available optical flow drivers
+		power_monitor/ina226
+		pwm_input
+		pwm_out_sim
+		px4fmu
+		px4io
+		rc_input
+		roboclaw
+		stm32
+		stm32/adc
+		stm32/armv7-m_dcache
+		stm32/tone_alarm
+		tap_esc
+		telemetry # all available telemetry drivers
+		tone_alarm
+		uavcan
+
+	MODULES
+		attitude_estimator_q
+		camera_feedback
+		commander
+		dataman
+		ekf2
+		events
+		land_detector
+		landing_target_estimator
+		load_mon
+		local_position_estimator
+		logger
+		mavlink
+		mc_att_control
+		mc_pos_control
+		navigator
+		sensors
+		sih
+		vmount
+		wind_estimator
+
+	SYSTEMCMDS
+		bl_update
+		config
+		dmesg
+		dumpfile
+		esc_calib
+		hardfault_log
+		led_control
+		mixer
+		motor_ramp
+		motor_test
+		mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		reflect
+		sd_bench
+		shutdown
+		top
+		topic_listener
+		tune_control
+		usb_connected
+		ver
+	)

boards/px4/fmu-v5x/rover.cmake
@@ -1,0 +1,102 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR px4
+	MODEL fmu-v5x
+	LABEL rover
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m7
+	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	UAVCAN_INTERFACES 2
+
+	SERIAL_PORTS
+		GPS1:/dev/ttyS1
+		TEL1:/dev/ttyS6
+		TEL2:/dev/ttyS4
+		TEL3:/dev/ttyS2
+		GPS2:/dev/ttyS0
+
+	DRIVERS
+		barometer # all available barometer drivers
+		batt_smbus
+		camera_capture
+		camera_trigger
+		distance_sensor # all available distance sensor drivers
+		gps
+		#heater
+		imu/adis16448
+		imu/adis16497
+		#imu # all available imu drivers
+# TBD		imu/bmi088 - needs bus selection
+# TBD		imu/ism330dlc - needs bus selection
+		imu/mpu6000
+		irlock
+		lights/blinkm
+		lights/oreoled
+		lights/pca8574
+		lights/rgbled
+		lights/rgbled_ncp5623c
+		magnetometer # all available magnetometer drivers
+		#md25
+		mkblctrl
+		optical_flow # all available optical flow drivers
+		pca9685
+		power_monitor/ina226
+		#protocol_splitter
+		pwm_input
+		pwm_out_sim
+		px4fmu
+		px4io
+		rc_input
+		roboclaw
+		stm32
+		stm32/adc
+		stm32/armv7-m_dcache
+		stm32/tone_alarm
+		telemetry # all available telemetry drivers
+		tone_alarm
+		uavcan
+
+	MODULES
+		camera_feedback
+		commander
+		dataman
+		ekf2
+		events
+		gnd_att_control
+		gnd_pos_control
+		land_detector
+		load_mon
+		logger
+		mavlink
+		navigator
+		sensors
+		vmount
+
+	SYSTEMCMDS
+		bl_update
+		config
+		dmesg
+		dumpfile
+		esc_calib
+		hardfault_log
+		led_control
+		mixer
+		motor_ramp
+		motor_test
+		mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		reflect
+		sd_bench
+		shutdown
+		top
+		topic_listener
+		tune_control
+		usb_connected
+		ver
+	)

boards/px4/fmu-v5x/rtps.cmake
@@ -1,0 +1,131 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR px4
+	MODEL fmu-v5x
+	LABEL rtps
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m7
+	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
+	UAVCAN_INTERFACES 2
+
+	SERIAL_PORTS
+		GPS1:/dev/ttyS1
+		TEL1:/dev/ttyS6
+		TEL2:/dev/ttyS4
+		TEL3:/dev/ttyS2
+		GPS2:/dev/ttyS0
+
+	DRIVERS
+		barometer # all available barometer drivers
+		batt_smbus
+		camera_capture
+		camera_trigger
+		differential_pressure # all available differential pressure drivers
+		distance_sensor # all available distance sensor drivers
+		gps
+		#heater
+		imu/adis16448
+		imu/adis16497
+		#imu # all available imu drivers
+# TBD		imu/bmi088 - needs bus selection
+# TBD		imu/ism330dlc - needs bus selection
+		imu/mpu6000
+		irlock
+		lights/blinkm
+		lights/oreoled
+		lights/pca8574
+		lights/rgbled
+		lights/rgbled_ncp5623c
+		magnetometer # all available magnetometer drivers
+		#md25
+		mkblctrl
+		optical_flow # all available optical flow drivers
+		pca9685
+		power_monitor/ina226
+		protocol_splitter
+		pwm_input
+		pwm_out_sim
+		px4fmu
+		px4io
+		rc_input
+		roboclaw
+		stm32
+		stm32/adc
+		stm32/armv7-m_dcache
+		stm32/tone_alarm
+		tap_esc
+		telemetry # all available telemetry drivers
+		test_ppm
+		tone_alarm
+		uavcan
+
+	MODULES
+		attitude_estimator_q
+		camera_feedback
+		commander
+		dataman
+		ekf2
+		events
+		fw_att_control
+		fw_pos_control_l1
+		gnd_att_control
+		gnd_pos_control
+		land_detector
+		landing_target_estimator
+		load_mon
+		local_position_estimator
+		logger
+		mavlink
+		mc_att_control
+		mc_pos_control
+		micrortps_bridge
+		navigator
+		sensors
+		sih
+		vmount
+		vtol_att_control
+		wind_estimator
+
+	SYSTEMCMDS
+		bl_update
+		config
+		dmesg
+		dumpfile
+		esc_calib
+		hardfault_log
+		led_control
+		mixer
+		motor_ramp
+		motor_test
+		mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		reflect
+		sd_bench
+		shutdown
+		tests # tests and test runner
+		top
+		topic_listener
+		tune_control
+		usb_connected
+		ver
+
+	EXAMPLES
+		bottle_drop # OBC challenge
+		fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		hello
+		hwtest # Hardware test
+		#matlab_csv_serial
+		position_estimator_inav
+		px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		rover_steering_control # Rover example app
+		segway
+		uuv_example_app
+	)

boards/px4/fmu-v5x/stackcheck.cmake
@@ -1,0 +1,130 @@
+
+px4_add_board(
+	PLATFORM nuttx
+	VENDOR px4
+	MODEL fmu-v5x
+	LABEL stackcheck
+	TOOLCHAIN arm-none-eabi
+	ARCHITECTURE cortex-m7
+	ROMFSROOT px4fmu_common
+	IO px4_io-v2_default
+	TESTING
+	#UAVCAN_INTERFACES 2
+
+	SERIAL_PORTS
+		GPS1:/dev/ttyS1
+		TEL1:/dev/ttyS6
+		TEL2:/dev/ttyS4
+		TEL3:/dev/ttyS2
+		GPS2:/dev/ttyS0
+
+	DRIVERS
+		barometer # all available barometer drivers
+		batt_smbus
+		camera_capture
+		camera_trigger
+		differential_pressure # all available differential pressure drivers
+		distance_sensor # all available distance sensor drivers
+		gps
+		#heater
+		imu/adis16448
+		imu/adis16497
+		#imu # all available imu drivers
+# TBD		imu/bmi088 - needs bus selection
+# TBD		imu/ism330dlc - needs bus selection
+		imu/mpu6000
+		#irlock
+		#lights/blinkm
+		#lights/oreoled
+		lights/pca8574
+		lights/rgbled
+		#lights/rgbled_ncp5623c
+		magnetometer # all available magnetometer drivers
+		#md25
+		mkblctrl
+		optical_flow # all available optical flow drivers
+		pca9685
+		power_monitor/ina226
+		#protocol_splitter
+		pwm_input
+		pwm_out_sim
+		px4fmu
+		px4io
+		rc_input
+		roboclaw
+		stm32
+		stm32/adc
+		stm32/armv7-m_dcache
+		stm32/tone_alarm
+		tap_esc
+		telemetry # all available telemetry drivers
+		test_ppm
+		tone_alarm
+		#uavcan
+
+	MODULES
+		attitude_estimator_q
+		camera_feedback
+		commander
+		dataman
+		ekf2
+		events
+		fw_att_control
+		fw_pos_control_l1
+		gnd_att_control
+		gnd_pos_control
+		land_detector
+		landing_target_estimator
+		load_mon
+		local_position_estimator
+		logger
+		mavlink
+		mc_att_control
+		mc_pos_control
+		navigator
+		sensors
+		sih
+		vmount
+		vtol_att_control
+		wind_estimator
+
+	SYSTEMCMDS
+		bl_update
+		config
+		dmesg
+		dumpfile
+		esc_calib
+		hardfault_log
+		led_control
+		mixer
+		motor_ramp
+		motor_test
+		mtd
+		nshterm
+		param
+		perf
+		pwm
+		reboot
+		reflect
+		sd_bench
+		shutdown
+		tests # tests and test runner
+		top
+		topic_listener
+		tune_control
+		usb_connected
+		ver
+
+	EXAMPLES
+		#bottle_drop # OBC challenge
+		#fixedwing_control # Tutorial code from https://px4.io/dev/example_fixedwing_control
+		#hello
+		#hwtest # Hardware test
+		#matlab_csv_serial
+		#position_estimator_inav
+		#px4_mavlink_debug # Tutorial code from http://dev.px4.io/en/debug/debug_values.html
+		#px4_simple_app # Tutorial code from http://dev.px4.io/en/apps/hello_sky.html
+		#rover_steering_control # Rover example app
+		#segway
+		#uuv_example_app
+	)