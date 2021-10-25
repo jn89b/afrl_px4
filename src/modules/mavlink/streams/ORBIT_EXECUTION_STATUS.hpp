wpwebrobotics/PX4-Autopilot

support orbit command in fixed wing mode
Signed-off-by: RomanBapst <bapstroman@gmail.com>

commander: support orbit mode for fixed wings

Signed-off-by: RomanBapst <bapstroman@gmail.com>

FwPositionControl: publish orbit status

Signed-off-by: RomanBapst <bapstroman@gmail.com>

commander:reject orbit mode while doing a vtol transition

Signed-off-by: RomanBapst <bapstroman@gmail.com>

FixedWingPositionControl: explicitly cast waypoint for Orbit status

FixedwingPositionControl: fill missing orbit_status fields

navigator_main: handle reposition/orbit corner cases

- set orbit rotation direction correctly
- send mavlink message when orbit is rejected

FixedWingPositionControl: correctly report rotation direction in orbit_status

navigator: hack to not break orbit while doing altitude changes

Signed-off-by: RomanBapst <bapstroman@gmail.com>

navigator: set cruise throttle for orbit command

Signed-off-by: RomanBapst <bapstroman@gmail.com>

src/modules/mavlink/streams/ORBIT_EXECUTION_STATUS.hpp
@@ -55,28 +55,32 @@ public:
 private:
 	explicit MavlinkStreamOrbitStatus(Mavlink *mavlink) : MavlinkStream(mavlink) {}
 
-	uORB::Subscription _orbit_status_sub{ORB_ID(orbit_status)};
+	uORB::SubscriptionMultiArray<orbit_status_s> _orbit_status_sub{ORB_ID::orbit_status};
 
 	bool send() override
 	{
-		orbit_status_s orbit_status;
+		orbit_status_s _orbit_status;
 
-		if (_orbit_status_sub.update(&orbit_status)) {
-			mavlink_orbit_execution_status_t msg{};
+		for (auto &orbit_sub : _orbit_status_sub) {
 
-			msg.time_usec = orbit_status.timestamp;
-			msg.radius = orbit_status.radius;
-			msg.frame = orbit_status.frame;
-			msg.x = orbit_status.x * 1e7;
-			msg.y = orbit_status.y * 1e7;
-			msg.z = orbit_status.z;
+			if (orbit_sub.update(&_orbit_status)) {
+				mavlink_orbit_execution_status_t _msg_orbit_execution_status{};
 
-			mavlink_msg_orbit_execution_status_send_struct(_mavlink->get_channel(), &msg);
+				_msg_orbit_execution_status.time_usec = _orbit_status.timestamp;
+				_msg_orbit_execution_status.radius = _orbit_status.radius;
+				_msg_orbit_execution_status.frame = _orbit_status.frame;
+				_msg_orbit_execution_status.x = _orbit_status.x * 1e7;
+				_msg_orbit_execution_status.y = _orbit_status.y * 1e7;
+				_msg_orbit_execution_status.z = _orbit_status.z;
 
-			return true;
+				mavlink_msg_orbit_execution_status_send_struct(_mavlink->get_channel(), &_msg_orbit_execution_status);
+
+				// only one subscription should ever be active at any time, so we can exit here
+				break;
+			}
 		}
 
-		return false;
+		return true;
 	}
 };
 

jn89b/afrl_px4

Former-commit-id: d84b0296d20ef1640a722b6070161d0c465d9362

src/modules/mavlink/streams/ORBIT_EXECUTION_STATUS.hpp
@@ -49,34 +49,40 @@ public:
 
 	unsigned get_size() override
 	{
-		return _orbit_status_sub.advertised() ? MAVLINK_MSG_ID_ORBIT_EXECUTION_STATUS_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES : 0;
+		return _orbit_status_subs.advertised() ? MAVLINK_MSG_ID_ORBIT_EXECUTION_STATUS_LEN + MAVLINK_NUM_NON_PAYLOAD_BYTES : 0;
 	}
 
 private:
 	explicit MavlinkStreamOrbitStatus(Mavlink *mavlink) : MavlinkStream(mavlink) {}
 
-	uORB::Subscription _orbit_status_sub{ORB_ID(orbit_status)};
+	uORB::SubscriptionMultiArray<orbit_status_s> _orbit_status_subs{ORB_ID::orbit_status};
 
 	bool send() override
 	{
 		orbit_status_s orbit_status;
+		bool updated = false;
 
-		if (_orbit_status_sub.update(&orbit_status)) {
-			mavlink_orbit_execution_status_t msg{};
+		for (auto &orbit_sub : _orbit_status_subs) {
 
-			msg.time_usec = orbit_status.timestamp;
-			msg.radius = orbit_status.radius;
-			msg.frame = orbit_status.frame;
-			msg.x = orbit_status.x * 1e7;
-			msg.y = orbit_status.y * 1e7;
-			msg.z = orbit_status.z;
+			if (orbit_sub.update(&orbit_status)) {
+				updated = true;
+				mavlink_orbit_execution_status_t msg_orbit_execution_status{};
 
-			mavlink_msg_orbit_execution_status_send_struct(_mavlink->get_channel(), &msg);
+				msg_orbit_execution_status.time_usec = orbit_status.timestamp;
+				msg_orbit_execution_status.radius = orbit_status.radius;
+				msg_orbit_execution_status.frame = orbit_status.frame;
+				msg_orbit_execution_status.x = orbit_status.x * 1e7;
+				msg_orbit_execution_status.y = orbit_status.y * 1e7;
+				msg_orbit_execution_status.z = orbit_status.z;
 
-			return true;
+				mavlink_msg_orbit_execution_status_send_struct(_mavlink->get_channel(), &msg_orbit_execution_status);
+
+				// only one subscription should ever be active at any time, so we can exit here
+				break;
+			}
 		}
 
-		return false;
+		return updated;
 	}
 };
 