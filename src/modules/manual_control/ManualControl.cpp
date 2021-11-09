samchamberlin/PX4-Autopilot

manual_control: don't switch gear on first init
This will ignore the first transition from NONE to something.
This matches the behaviour that existed before.

src/modules/manual_control/ManualControl.cpp
@@ -230,7 +230,8 @@ void ManualControl::Run()
 						}
 					}
 
-					if (switches.gear_switch != _previous_switches.gear_switch) {
+					if (switches.gear_switch != _previous_switches.gear_switch
+					    && _previous_switches_.gear_switch != manual_control_switches_s::SWITCH_POS_NONE) {
 
 						if (switches.gear_switch == manual_control_switches_s::SWITCH_POS_ON) {
 							publish_landing_gear(landing_gear_s::GEAR_UP);

jn89b/afrl_px4

Former-commit-id: cfdb53a4d88efe65c9ee788a7474e8d55001d5c3

src/modules/manual_control/ManualControl.cpp
@@ -230,7 +230,8 @@ void ManualControl::Run()
 						}
 					}
 
-					if (switches.gear_switch != _previous_switches.gear_switch) {
+					if (switches.gear_switch != _previous_switches.gear_switch
+					    && _previous_switches.gear_switch != manual_control_switches_s::SWITCH_POS_NONE) {
 
 						if (switches.gear_switch == manual_control_switches_s::SWITCH_POS_ON) {
 							publish_landing_gear(landing_gear_s::GEAR_UP);